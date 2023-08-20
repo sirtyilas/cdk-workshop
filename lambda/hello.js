e9bdf62fbd19aa1cd7f8284a187547b31765d445
lmatyila <lmatyila@mit.edu>
2023-08-20 19:50:52 +0200

    add proper greeting

--- lambda/hello.js
+++ lambda/hello.js
@@ -3,7 +3,7 @@ exports.handler = async function (event) {
 	return {
 		statusCode: 200,
 		headers: { "Content-Type": "text/plain" },
-		body: `Good Night, CDK! You've hit ${event.path}\n`
+		body: `Nice to meet you, CDK! You've hit ${event.path}\n`
 	}
 
 
